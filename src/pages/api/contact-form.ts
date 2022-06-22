edf06f0a6181f5751de2f77f5fea002da38a64c2
iliampav <iliamfer@hotmail.com>
2022-06-22 17:45:01 -0300

    add form

--- src/pages/api/contact-form.ts
+++ src/pages/api/contact-form.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import sendgrid from "@sendgrid/mail";
 
 interface RequestData extends NextApiRequest{
   body: {
@@ -18,6 +19,29 @@ export default function handler(req: RequestData, res: NextApiResponse) {
       // Sends a HTTP bad request error code
       return res.status(400).json({ data: 'Something is not filled' })
     }
+
+    const sendgridApi = process.env.SENDGRID_API_KEY;
+    
+    if(!sendgridApi) {
+      throw new Error()
+    } 
+
+    sendgrid.setApiKey(sendgridApi)
+
+    const msg = {
+      to: 'iliamfer@hotmail.com', 
+      from: 'iliamfer@hotmail.com',
+      subject: 'Mensagem do portifolio',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+
+    sendgrid.send(msg).then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+    })
 
     res.status(200).json({ data: `${body.name} ${body.email} ${body.message}` })
   }
