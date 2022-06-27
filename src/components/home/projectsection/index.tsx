fb28e57d5cf4c099bea1bfa64ca460c80837c86e
iliampav <iliamfer@hotmail.com>
2022-06-27 10:56:42 -0300

    banner changes

--- src/components/home/projectsection/index.tsx
+++ src/components/home/projectsection/index.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import { useEffect, useRef } from 'react'
-import Draggable from 'gsap/Draggable'
 
 import styles from './style.module.scss'
 
@@ -9,52 +8,55 @@ import signatureImg from '@/public/assets/img/home/projectssection/signatureimg.
 import tincoimg from '@/public/assets/img/home/projectssection/tincoimg.png'
 import yahcafeimg from '@/public/assets/img/home/projectssection/yahcafeimg.png'
 
-import gsap from 'gsap'
-
-gsap.registerPlugin(Draggable);
+import gsap from "gsap";
 
 export default function Projects() {
 
-    // const itxpic = useRef<HTMLAnchorElement>(null)
+    const slider = useRef(null)
 
-    const zoomIn = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
+    const zoomIn = (e:React.MouseEvent<HTMLElement>) => {
+        e.preventDefault()
         
-        gsap.to(e.currentTarget, {
-            scale: 1.2, 
-            width: '-=35%'
-        })
+        gsap.to(e.currentTarget, { scale: 1.2, width: '-=35%', height: '+=20%'})
     }
 
-    const zoomOut = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
+    const zoomOut = (e:React.MouseEvent<HTMLElement>) => {
+        e.preventDefault()
         
-        gsap.to(e.currentTarget, {scale: 1.0, width: '+=35%', })
+        gsap.to(e.currentTarget, {scale: 1.0, width: '+=35%', height: '-=20%' })
     }
 
-    const sliderRef = useRef(null);
+    const changeCarrousselImg = (e:React.MouseEvent<HTMLElement>) => {
+        e.preventDefault()
+
+        const bannerPostion = e.currentTarget.dataset.position
+
+        switch(bannerPostion) {
+            case 'center':
+                console.log('banner ativo')
+                break;
+            case 'left':
+                console.log('banner left')
+                break;
+            case 'right':
+                console.log('banner right')
+                break;
+        }
+    }
 
     // test carroussel
-
-    useEffect(() => {
-        console.log(sliderRef.current.clientWidth, sliderRef.current.innerWidth);
-        Draggable.create(sliderRef.current, {
-          type: "x",
-          bounds: {
-            minX: -sliderRef.current.clientWidth + window.innerWidth * 0.88,
-            maxX: 0
-          }
-        });
-    }, []);
 
     return (
         <section className={styles.projectsSection}
                 onMouseEnter={zoomIn}
                 onMouseLeave={zoomOut}
+                ref={slider}
                  >
             <a 
-            className={styles.active}>
-            {/* onMouseEnter={zoomOut}> */}
+            className={styles.active}
+            onClick={changeCarrousselImg}
+            data-position='center'
+                >
                 <h1>PROJECT</h1>
                 <h1>ITX</h1>
                 <Image
@@ -65,7 +67,10 @@ export default function Projects() {
                     layout="fill" 
                 />
             </a>
-            <a id='signatureImg' className={styles.left}>
+            <a id='signatureImg' className={styles.left}
+            onClick={changeCarrousselImg}
+            data-position='left'
+                >
                 <h1>PROJECT</h1>
                 <h1>SIGNATURE</h1>
                 <Image
@@ -77,7 +82,10 @@ export default function Projects() {
                     layout="fill" 
                 />
             </a>
-            <a id='tincoimg' className={styles.right}> 
+            <a id='tincoimg' className={styles.right}
+            onClick={changeCarrousselImg}
+            data-position='right'
+                > 
                 <h1>PROJECT</h1>
                 <h1>TINCO</h1>
                 <Image
@@ -88,7 +96,9 @@ export default function Projects() {
                     layout="fill" 
                 />
             </a>
-            <a id='yahcafeimg' className={styles.last}>
+            <a id='yahcafeimg' className={styles.last}
+            onClick={changeCarrousselImg}
+            data-position='none'>
                 <h1>PROJECT</h1>
                 <h1>YAH CAFÉ</h1>
                 <Image
@@ -99,6 +109,12 @@ export default function Projects() {
                     layout="fill" 
                 />
             </a>
+            <a className={styles.nextBanner}
+               data-position='right'
+            ></a>
+            <a className={styles.prevBanner}
+               data-position='left'
+            ></a>
         </section>
     )
 }
