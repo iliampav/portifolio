f21dd054515710a02fb80ac69606caca5253061c
iliampav <iliamfer@hotmail.com>
2022-06-30 16:52:05 -0300

    added some changes in experience responsive

--- src/components/about/somexp/index.tsx
+++ src/components/about/somexp/index.tsx
@@ -3,11 +3,11 @@ import styles from './styles.module.scss'
 export default function Experience() {
     return (
         <section className={styles.experience}>
-            <p>For the last 8 years I’ve been <br/>
-                sharpening my skills in <br/>
-                design, digital marketing,<br/>
-                seo and, more recently <br/>
-                <strong>building products for web.</strong><br/>
+            <p>For the last 8 years I’ve been 
+                sharpening my skills in 
+                design, digital marketing,
+                seo and, more recently <br />
+                <strong> building products for web.</strong>
             </p>
             <div className={styles.bgImg}>
 
