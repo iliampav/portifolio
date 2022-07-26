9f87ba6e2a1836711ced442e463473fb720dbd35
iliampav <iliamfer@hotmail.com>
2022-07-25 21:29:03 -0300

    cruzeboat animation

--- src/components/home/banner/banner-sail/sailanimation/index.tsx
+++ src/components/home/banner/banner-sail/sailanimation/index.tsx
@@ -1,4 +1,4 @@
-import Cruze from './images/cruze.js'
+import Cruze from './images/cruze'
 import WaveBoat from '@/public/assets/img/home/bannerimages/sail/wave1.svg'
 import PavkoLogo from '@/public/assets/img/home/bannerimages/sail/pavko.svg'
 import Bird from '@/public/assets/img/home/bannerimages/sail/birds.svg'
@@ -9,25 +9,8 @@ import Boat from '@/public/assets/img/home/bannerimages/sail/group.svg'
 import Boatsmall from '@/public/assets/img/home/bannerimages/sail/boat2.svg'
 import Girlsea from '@/public/assets/img/home/bannerimages/sail/girlsea.svg'
 import styles from './styles.module.scss';
-import {gsap, Back} from 'gsap'
-import { useEffect, useRef } from 'react'
-
-let tlConfig: GSAPTimelineVars = {
-    paused: true
-};
 
 export default function sailAnimation() {
-
-    const cruzeBoat = useRef<HTMLHeadingElement>(null)
-
-    const cruzeTl = gsap.timeline(tlConfig).play()
-
-    useEffect(() => {
-        console.log(cruzeBoat.current)
-        cruzeTl.from(cruzeBoat.current, { left: '100%', width: '-=5%', top: "50%", duration: 50})
-               .to(cruzeBoat.current, { ease: Back.easeInOut.config(.7), top: "-=5px", repeat:-1, duration:1.5});
-        
-    }, [])
 
     return (
         <>
@@ -35,7 +18,7 @@ export default function sailAnimation() {
                 <div className={styles.animationborder}>
                     <WaveBoat className={styles.boatwaves}/>
                     {/* <Cruze className={styles.cruzePosition}/> */}
-                    <Cruze setRef={cruzeBoat} className={styles.cruzePosition}/>
+                    <Cruze/>
                     <PavkoLogo className={styles.pavkoLogo} />
                 </div>
             </div>
