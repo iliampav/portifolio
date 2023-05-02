a21ece1039d75ae045868709a117472f055e3b46
iliampav <iliamfer@hotmail.com>
2023-05-02 14:34:40 -0300

    changes on dash

--- src/pages/quicksight.js
+++ src/pages/quicksight.js
@@ -29,12 +29,12 @@ function generateEmbedUrlForAnonymousUser(
     };
 
     const quicksightClient = new AWS.QuickSight({
-        region: process.env.AWS_REGION,
+        region: 'us-east-1',
     });
 
     quicksightClient.generateEmbedUrlForAnonymousUser(generateEmbedUrlForAnonymousUserParams, function(err, data) {
         if (err) {
-            console.log(err, err.stack);
+            console.log(err);
         } else {
             const result = {
                 "statusCode": 200,
@@ -51,11 +51,11 @@ function generateEmbedUrlForAnonymousUser(
 }
 
 let embededUrl = generateEmbedUrlForAnonymousUser(
-    '0784-9604-0109', 
+    '078496040109', 
     '7c894cb9-39ba-4fd9-bda8-f02993321d62', 
     'default',
     ['arn:aws:quicksight:us-east-1:078496040109:dashboard/7c894cb9-39ba-4fd9-bda8-f02993321d62'],
-    ['FIPAL - MITSUBISHI'],
+    [{ 'Key': 'name', 'Value': 'FIPAL - MITSUBISHI'}],
 )
 
 console.log(embededUrl)
